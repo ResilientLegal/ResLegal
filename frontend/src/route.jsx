f93cdd70135d4a5de4ebdf6b7f399122fb25823c
pichsorita <pichsoritayim@gmail.com>
2025-11-15 21:17:04 -0800

    Add matter Record

--- frontend/src/route.jsx
+++ frontend/src/route.jsx
@@ -1,13 +1,16 @@
 // import MattersPage from './pages/MattersPage'
 import MattersList from './pages/MattersList';
 import HomePage from './pages/HomePage'
-
+import MatterForm from './components/MatterForm';
 const routes = [
     {
         path: '/', component:  <HomePage /> 
     },
     {
         path: '/matters', component: <MattersList /> 
+    },
+    {
+        path: '/matter', component: <MatterForm />
     }
 ]
 
