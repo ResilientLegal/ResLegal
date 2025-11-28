d97d29e30057f3b52d328b577a868cf7b8c8dbe5
pichsorita <pichsoritayim@gmail.com>
2025-11-27 19:45:08 -0800

    Add example for Rest API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,74 @@
+import json
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Matter
 from .serializers import MatterSerializer
 
+GRAPHQL_URL = "http://localhost:8000/graphql"
+REST_URL = "http://localhost:18000/v1/transactions"
+
 class MatterViewSet(viewsets.ModelViewSet):
     queryset = Matter.objects.all()
     serializer_class = MatterSerializer
+
+
+class CommitTransaction(APIView):
+    # def postGraphQl(self, request):
+    #     metadata = request.data["metadata"]
+    #     asset = request.data["asset"]
+
+    #     query = f"""
+    #     mutation {{
+    #       postTransaction(data: {{
+    #         operation: "CREATE",
+    #         amount: 1,
+    #         signerPublicKey: "{metadata['signerPublicKey']}",
+    #         signerPrivateKey: "{metadata['signerPrivateKey']}",
+    #         recipientPublicKey: "{metadata['recipientPublicKey']}",
+    #         asset: {{
+    #           data: {asset}
+    #         }}
+    #       }}) {{
+    #         id
+    #       }}
+    #     }}
+    #     """
+
+    #     r = requests.post(GRAPHQL_URL, json={"query": query})
+    #     return Response(r.json())
+    
+    def post(self, request):
+        r = requests.post(REST_URL + '/commit', headers={"Content-Type": "application/json"}, data=json.dumps(request.data), timeout=10)
+        return Response(r)
+        
+
+
+class GetTransaction(APIView):
+    # def get(self, request, txn_id):
+    #     query = """
+    #     query GetTx($id: ID!) {
+    #         getTransaction(id: $id) {
+    #             id
+    #             version
+    #             amount
+    #             uri
+    #             type
+    #             publicKey
+    #             operation
+    #             metadata
+    #             asset
+    #             signerPublicKey
+    #         }
+    #     }
+    #     """
+
+    #     r = requests.post(GRAPHQL_URL, json={"query": query, "variables": {"id": txn_id}})
+    #     return Response(r.json())
+
+    def get(self, request, txn_id):
+        r = requests.get(REST_URL + f'/{txn_id}', headers={"Content-Type": "application/json"})
+        if (r):
+            return Response(r.json())
--- backend/reslegal/urls.py
+++ backend/reslegal/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from api.views import MatterViewSet
+from api.views import MatterViewSet, CommitTransaction, GetTransaction
 
 router = routers.DefaultRouter()
 router.register(r'matters', MatterViewSet)
@@ -26,4 +26,6 @@ router.register(r'matters', MatterViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path("api/transactions/commit", CommitTransaction.as_view()),
+    path("api/transactions/<str:txn_id>/", GetTransaction.as_view()),
 ]
