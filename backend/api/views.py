0c1764502d056f2a45532ab5ae81a4639d3ab239
Pichsorita Yim <pichsoritayim@gmail.com>
2025-12-04 14:15:41 -0800

    Revert "add error handling"
    This reverts commit ef07d9067c30953f1d87ace75700dd3e2f744cc4.

--- backend/api/views.py
+++ backend/api/views.py
@@ -126,7 +126,7 @@ class LoginView(APIView):
         }
         resdb_tx_id = save_to_resilientdb(resdb_data)
 
-        return Response({
+       return Response({
             'token': str(refresh.access_token),
             'refresh': str(refresh),
             'user': {
